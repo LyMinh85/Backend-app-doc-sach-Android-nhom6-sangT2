2d425bbdd234efec3ce7cc59cbaafd5441dd91b7
LyMinh85 <mlym8565@gmail.com>
2024-05-09 13:44:10 +0700

    Add response type for api/luot-doc.

--- src/api/luot-doc/dto/luot-doc.dto.ts
+++ src/api/luot-doc/dto/luot-doc.dto.ts
@@ -1,9 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { NguoiDung } from 'src/api/nguoi-dung/entities/nguoi-dung.entity';
 import { SachDto } from 'src/api/sach/dto/sach.dto';
 
 export class LuotDocDTO {
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   sachDto: SachDto;
+
+  @ApiProperty()
   nguoiDung: NguoiDung;
   constructor(luotDoc: Partial<LuotDocDTO>) {
     this.id = luotDoc.id;
